b17cb1041fbeb1f70d77ee83c60d34f7ecb69d9b
Davy Liebe <107366486+SLAMNOTH@users.noreply.github.com>
2023-11-30 10:15:27 +0100

    cam follow + movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,19 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
-   void LateUpdate()
-{
-    if (target != null)
+    void LateUpdate()
     {
-        Debug.Log("Target position: " + target.position);
+        if (target != null)
+        {
+            // Keep the same y-coordinate as the camera's current position
+            float newY = transform.position.y;
 
-        Vector3 desiredPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+            // Calculate the desired position with the new y-coordinate
+            Vector3 desiredPosition = new Vector3(target.position.x + offset.x, newY, transform.position.z);
+
+            // Smoothly move the camera to the desired position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
     }
-}
-
 }
